28534a14c878bb1116def61a2833807a7fb391e3
FlackAP <flack.ap@gmail.com>
2014-03-25 15:06:10 -0400

    added method for starting a new round added variable for target value and added value of _targetValue to alert message housekeeping n shit

--- Bullseye/APFViewController.m
+++ Bullseye/APFViewController.m
@@ -14,12 +14,14 @@
 
 @implementation APFViewController {
     int _currentValue;
+    int _targetValue;
 }
 
+//when view loads, set current value and add a pseudo-random number between 1 and 100 based on startNewRound method
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	_currentValue = self.slider.value;
+    [self startNewRound];
 }
 
 - (void)didReceiveMemoryWarning
@@ -30,7 +32,9 @@
 
 - (IBAction)showAlert {
     NSString *message = [NSString stringWithFormat:
-                         @"The value of the slider is: %d", _currentValue];
+                         @"The value of the slider is: %d\nThe target value is: %d",
+                         _currentValue, _targetValue];
+    
     UIAlertView *alertView = [[UIAlertView alloc]
                               initWithTitle:@"Hello, World!"
                               message:message
@@ -39,8 +43,21 @@
                               otherButtonTitles:nil];
     [alertView show];
     
+    
+    //starts a new round after alert is shown
+    [self startNewRound];
+    
 }
 
+//method to create a new round- to be performed when user clicks "click me!"
+- (void)startNewRound
+{
+    _targetValue = 1 + arc4random_uniform(100);
+    _currentValue = self.slider.value;
+    self.slider.value = _currentValue;
+}
+
+//method to check the slider's value and round it to closest integer
 - (IBAction)sliderMoved:(UISlider *)slider {
     
         _currentValue = lroundf(slider.value);
